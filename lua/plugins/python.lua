39a4f0e1326e878e3e972681b39d09d5ddf9ecc5
arcashka <pewpewstol@gmail.com>
2023-09-19 08:20:58 +0300

    pylsp changes

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -1,15 +1,16 @@
-local util = require("lspconfig/util")
-
+local has_npm = vim.fn.system("command -v npm") ~= ""
+local servers = has_npm and {
+    ---@type lspconfig.options.pyright
+    pyright = {},
+  } or {
+    ---@type lspconfig.options.pylsp
+    pylsp = {},
+  }
 return {
   {
     "neovim/nvim-lspconfig",
     opts = {
-      servers = {
-        ---@type lspconfig.options.pyright
-        pyright = {},
-        -- ---@type lspconfig.options.pylsp
-        -- pylsp = {},
-      },
+      servers = servers,
     },
   },
 }
