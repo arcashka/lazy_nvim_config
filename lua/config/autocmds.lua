183f2321b8d327fdb934a87db1b54f3925a0f3cc
arcashka <pewpewstol@gmail.com>
2024-02-02 08:48:50 +0200

    wgsl lsp

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -13,3 +13,17 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.shiftwidth = 2
   end,
 })
+
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = "*.wgsl",
+  callback = function()
+    vim.bo.filetype = "wgsl"
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "wgsl" },
+  callback = function()
+    vim.b.autoformat = false
+  end,
+})
