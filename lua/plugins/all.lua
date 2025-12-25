2617c51d950101491f553ebd6aefee3528e9d5e5
arcashka <pewpewstol@gmail.com>
2025-12-25 16:57:02 +0200

    clean

--- lua/plugins/all.lua
+++ lua/plugins/all.lua
@@ -18,10 +18,10 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight-night",
+      colorscheme = "vscode",
     },
   },
-  -- { "Mofiqul/vscode.nvim" },
+  { "Mofiqul/vscode.nvim" },
   -- remove annoying notifications
   {
     "folke/noice.nvim",
@@ -62,19 +62,6 @@ return {
     config = true,
   },
 
-  {
-    "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
-    opts = function(_, opts)
-      ---@type lspconfig.options
-      opts.servers = {
-        jsonls = {
-          mason = false,
-        },
-      }
-    end,
-  },
-
   -- add more treesitter parsers
   {
     "nvim-treesitter/nvim-treesitter",
@@ -103,7 +90,7 @@ return {
 
   -- add any tools you want to have installed below
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = {
       ensure_installed = {
         "stylua",
@@ -111,118 +98,6 @@ return {
         "shfmt",
         "clangd",
         "cmakelint",
-      },
-    },
-  },
-  {
-    "mfussenegger/nvim-dap",
-    keys = function()
-      return {
-        {
-          "<F5>",
-          function()
-            require("dap").continue()
-          end,
-          desc = "Debug start/continue",
-        },
-        {
-          "<F10>",
-          function()
-            require("dap").step_over()
-          end,
-          desc = "Debug step over",
-        },
-        {
-          "<F9>",
-          function()
-            require("dap").step_into()
-          end,
-          desc = "Debug step into",
-        },
-        {
-          "<F11>",
-          function()
-            require("dap").step_out()
-          end,
-          desc = "Debug step out",
-        },
-        {
-          "<F2>",
-          function()
-            require("dap").toggle_breakpoint()
-          end,
-          desc = "Debug toggle breakpoint",
-        },
-        {
-          "<F3>",
-          function()
-            require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
-          end,
-          desc = "Debug set breakpoint with condition",
-        },
-        {
-          "<F4>",
-          function()
-            require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
-          end,
-          desc = "Debug set breakpoint log point message",
-        },
-        {
-          "<F6>",
-          function()
-            require("dap").repl.open()
-          end,
-          desc = "Debug open repl",
-        },
-        {
-          "<F7>",
-          function()
-            require("dap").run_last()
-          end,
-          desc = "Debug retun last",
-        },
-      }
-    end,
-  },
-  {
-    "rcarriga/nvim-dap-ui",
-    keys = function()
-      return {
-        {
-          "<F12>",
-          function()
-            require("dapui").toggle()
-          end,
-          desc = "Debug UI",
-        },
-      }
-    end,
-  },
-  {
-    "chentoast/marks.nvim",
-    opts = {
-      default_mappings = true,
-    },
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {
-      {
-        "nvim-telescope/telescope-live-grep-args.nvim",
-        version = "^1.0.0",
-      },
-    },
-    config = function(_, opts)
-      require("telescope").load_extension("live_grep_args")
-      require("telescope").setup(opts)
-    end,
-    keys = {
-      {
-        "<leader>fp",
-        function()
-          require("telescope").extensions.live_grep_args.live_grep_args()
-        end,
-        desc = "Grep with args",
       },
     },
   },
